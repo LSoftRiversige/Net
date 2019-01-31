393a3b0d9563fd0637026bdcc40ac50ad16ead47
lsoft.riverside <lsoft.riverside@gmail.com>
2019-01-31 14:49:26 +0200

    1

---
+++ CityGame/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CityGame
+{
+    public class Game
+    {
+        //public static List<Answer> Answers { get; set; } = new List<Answer>();
+    }
+
+    internal class Answer
+    {
+    }
+}
